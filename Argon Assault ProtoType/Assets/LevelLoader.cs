039bf8ea351415334d5e40608e69ed7aaefe9f45
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2018-01-15 20:54:49 -0600

    Main Game elements
    Textured the terrain and added a spacelike skybox, created a modular player ship, added a splash screen, added music to splash screen and main scene

---
+++ Argon Assault ProtoType/Assets/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadFirstScene", 8f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
