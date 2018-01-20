f52ae061998b157b3bf6a34746f39695e172f37d
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2018-01-19 18:39:32 -0600

    Camera and ship movement
    created and added scripts to control camera and ship movement

---
+++ Argon Assault ProtoType/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("Sets the X Range in m from center")] [SerializeField] float clampRange = 10f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -clampRange, clampRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
