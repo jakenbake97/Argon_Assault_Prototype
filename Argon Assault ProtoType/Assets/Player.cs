27d2fcff48aa77650b79b2253d5a946df0d8c019
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2018-01-19 21:20:24 -0600

    XY Movement and Rotation
    Added movement in Y direction
    Added rotation for pitch, yaw, and roll
    cleaned up code with single action methods

--- Argon Assault ProtoType/Assets/Player.cs
+++ Argon Assault ProtoType/Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -7,7 +8,16 @@ public class Player : MonoBehaviour
 {
 
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
-    [Tooltip("Sets the X Range in m from center")] [SerializeField] float clampRange = 10f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f;
+    [Tooltip("Sets the X Range in m from center")] [SerializeField] float xClampRange = 10f;
+    [Tooltip("Sets the -Y Range in m from center")] [SerializeField] float yMinClampRange = 10f;
+    [Tooltip("Sets the Y Range in m from center")] [SerializeField] float yMaxClampRange = 10f;
+    [Tooltip("Changes how much the ship pitches based on its Y position")] [SerializeField] float positionPitchFactor = -5f;
+    [Tooltip("Changes how much the ship pitches while moving in Y")] [SerializeField] float controlPitchFactor = -5f;
+    [Tooltip("Changes how much the ship yaws based on its X position")] [SerializeField] float positionYawFactor = 5f;
+    [Tooltip("Changes how much the ship rolls while moving in X")] [SerializeField] float controlRollFactor = 5f;
+
+    float xThrow, yThrow;
     // Use this for initialization
     void Start()
     {
@@ -17,11 +27,68 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = PitchRotation();
+        float yaw = YawRotation();
+        float roll = RollRotation();
+        ShipRotation(pitch, yaw, roll);
+    }
+
+    private void ShipRotation(float pitch, float yaw, float roll)
+    {
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private float RollRotation()
+    {
+        return xThrow * controlRollFactor;
+    }
+
+    private float YawRotation()
+    {
+        return transform.localPosition.x * positionYawFactor;
+    }
+
+    private float PitchRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        return pitch;
+    }
+
+    private void ProcessTranslation()
+    {
+        float clampedXPos = XPosMovement();
+        float clampedYPos = YPosMovement();
+        XYShipTransform(clampedXPos, clampedYPos);
+    }
+
+    private float XPosMovement()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
-        float clampedXPos = Mathf.Clamp(rawXPos, -clampRange, clampRange);
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+        return clampedXPos;
+    }
 
-        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    private float YPosMovement()
+    {
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, yMinClampRange, yMaxClampRange);
+        return clampedYPos;
+    }
+
+    private void XYShipTransform(float clampedXPos, float clampedYPos)
+    {
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
